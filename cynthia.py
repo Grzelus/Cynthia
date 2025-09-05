70c9ffe0805ffdb3ea40d0b54b5e6c7c22543ecd
Kacper Grzelak <grzelakdev@gmail.com>
2025-09-05 23:15:27 +0200

    commands, config, daily song function

--- cynthia.py
+++ cynthia.py
@@ -1,10 +1,37 @@
 import discord
+import datetime
 from discord.ext import commands, tasks
 import requests
 import os
 import random
 import asyncio
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from yt_dlp import YoutubeDL
+import json
+
+#CONFIG FILE AND ITS FUNCTION
+#####################################################
+CONFIG_FILE = "config.json"
+
+def save_config(playlist_url, channel_id):
+    data = {
+        "playlist_url": playlist_url,
+        "discord_text_channel": channel_id
+    }
+    with open(CONFIG_FILE, "w") as file:
+        json.dump(data, file)
+
+def load_config():
+    if not os.path.exists(CONFIG_FILE):
+        return None, None
+    with open(CONFIG_FILE, "r") as f:
+        data = json.load(f)
+        return data.get("playlist_url"), data.get("discord_text_channel")
+    
+playlist_url, discord_text_channel = load_config()
+#####################################################
+
 
 def createInteraction(ctx:commands.Context,member:discord.Member,title: str, description: str, color: discord.Color, urls: list[str], footerText:str,):
     imageID = random.randint(0,len(urls)-1)
@@ -22,16 +49,19 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 
 intents = discord.Intents.default()
-intents.message_content = True         # Needed to read message content
-intents.members = True                 # Needed to access member info (like mentions and roles)
-intents.guilds = True                  # Needed for basic server data
+intents.message_content = True        
+intents.members = True                 
+intents.guilds = True                  
 intents.reactions = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+    await daily_song()
 
+#INTERACTION COMMANDS
+#####################################################
 @bot.command()
 async def hello(ctx: commands.Context):
     urls = ["https://i.pinimg.com/originals/35/35/00/3535009c4a5e1d6c611dc436183b2be3.gif", "https://i.pinimg.com/originals/b5/99/6b/b5996befa70fc2fdf1379877f4488fec.gif", "https://i.pinimg.com/originals/97/24/d6/9724d6d1ae0f4e5018edc1a61fecafdc.gif"]
@@ -71,7 +101,7 @@ async def slap(ctx:commands.Context, member: discord.Member):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def marriage(ctx:commands.Context, member: discord.Member):
+async def marry(ctx:commands.Context, member: discord.Member):
     urls = ["https://i.pinimg.com/originals/01/91/f9/0191f989bab551e76a452b5c2fdf8c34.gif"]
     embed = createInteraction(ctx, member,"Ohh, it's getting serious!", f"{member.mention} do you accept it?",discord.Color.blurple(),urls,f"{ctx.author.display_name} proposes you something special")
 
@@ -93,8 +123,10 @@ async def marriage(ctx:commands.Context, member: discord.Member):
             await ctx.send(f"I'm sorry... {member.mention} don't accepted your proposal :c")
     except asyncio.TimeoutError:
          await ctx.send(f"{member.mention} didn't respond in time.")
+#####################################################
 
-
+#ADMINISTRATION COMMANDS
+#####################################################
 @bot.command()
 @commands.has_permissions(manage_roles=True)
 async def addrole(ctx: commands.Context, user: discord.Member, *, roleName: str):
@@ -165,10 +197,80 @@ async def unban(ctx: commands.Context, user: str):
             return
     
     await ctx.send(f"There isn't {user} in banned members.")
+#####################################################
+
+@bot.command()
+async def set_daily_playlist(ctx: commands.Context, link: str):
+    global playlist_url
+
+    if link is None:
+        await ctx.send("Please select your playlist!")
+        return
     
-        
+    playlist_url = link
+    save_config(playlist_url, discord_text_channel)
+    await ctx.send(f"Playlist link is set to {playlist_url}")
 
+@bot.command()
+async def set_daily_playlist_channel(ctx: commands.Context, channel_name):
+    global discord_text_channel
 
+    for channel in ctx.guild.channels:
+        if channel.name == channel_name:
+            discord_text_channel = channel.id
+            save_config(playlist_url, discord_text_channel)
+            break
+    
+    
+@tasks.loop(hours=24)
+async def daily_song():
+    global playlist_url, discord_text_channel
+
+    if not playlist_url or not discord_text_channel:
+        print("there no playlist or channel")
+        return
+    ydl_opts = {
+        'extract_flat': True,
+        'quiet': True,
+        'skip_download': True,
+    }
+
+    songs = []
+
+    try:
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(playlist_url, download=False)
+            if 'entries' not in info:
+                print("not a playlist")
+                return
+
+            for entry in info['entries']:
+                video_url = f"https://www.youtube.com/watch?v={entry['id']}"
+                songs.append(video_url)
+
+        song_url = random.choice(songs)
+        song_id = song_url.split("v=")[-1].split("&")[0] if "v=" in song_url else song_url.split("/")[-1]
+
+        def get_title(url):
+            try:
+                response = requests.get(url)
+                soup: BeautifulSoup = BeautifulSoup(response.text, 'html.parser')
+                return soup.title.string.strip()
+            except Exception as e:
+                print(f"Error fetching title: {e}")
+                return("Song of the day!")
+            
+        title = get_title(song_url)
+
+        embed = discord.Embed(title="Song of the day!",description=f"**{title}**\n[Watch on YouTube]({song_url})", color=discord.Color.blue(),url=song_url)
+        embed.set_image(url=f"https://img.youtube.com/vi/{song_id}/maxresdefault.jpg")
+        embed.set_footer(text="Your daily dose of music!")
+
+        channel = bot.get_channel(discord_text_channel)
+        await channel.send(embed=embed)
+    except Exception as e:
+        print(f"Error in daily song function")
 
 bot.run(DISCORD_TOKEN)
 
